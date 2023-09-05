eb9c3540d73f56ef51aa37814f260719c593bd1b
Агаларов Игорь <igorvippzone@gmail.com>
2023-09-05 11:13:29 +0300

    обработка запроса последней странницы

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,17 @@ function App() {
     const [todos, setTodos] = useState<ITodo[]>([])
     const [page, setPage] = useState(0)
     const [isLoading, setIsLoading] = useState(false)
+    const [isLastPage, setIsLastPage] = useState(false)
 
     const getTodos = async () => {
         try {
             setIsLoading(true)
             const nextPage = page + 1
             const result: AxiosResponse<ITodoFetch[]> = await axios.get(`https://jsonplaceholder.typicode.com/todos?_page=${nextPage}`)
+            if(result.data.length === 0){
+                setIsLastPage(true)
+                throw new Error('Ошибка, получен пустой массив')
+            }
             const todos = result.data.map(generateData)
             setTodos((prev) => [...prev, ...todos])
             setPage(nextPage)
@@ -37,6 +42,7 @@ function App() {
     }, [isLoading, getTodos])
 
     const scrollHandler = (e: React.UIEvent<HTMLDivElement>) => {
+        if(isLastPage) return
         const scrollHeight = e.currentTarget.scrollHeight
         const scrollTop = e.currentTarget.scrollTop
         const clientHeight = e.currentTarget.clientHeight
